07cdab7b9739bd862897dbb92aa16a05c3789748
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-02 14:51:10 +0100

    nuova repo creata tramite template git. creati file blade

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,13 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>titolo</title>
+    <title> @yield('title', 'title') </title>
 
     @vite('resources/js/app.js')
 </head>
 
 <body>
+    <header>
+        @include('partials.header')
+    </header>   
 
+    <main>
+        @yield('main-section')
+    </main>
+
+    <footer>
+        @include('partials.footer')
+    </footer>
 </body>
 
 </html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('title')
+esercizio
+@endsection
+
+
+@section('main-section')
+<p>main</p>
+@endsection
