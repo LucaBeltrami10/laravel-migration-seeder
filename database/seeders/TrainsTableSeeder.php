67887afc8b57dec7cb051168a6a92624c0bfe856
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-02 17:55:59 +0100

    test con seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,14 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
+
     public function run(): void
     {
-        //
+        $trainsssssss = [
+            'id' => 1,
+            'company' => 'trenitalia',
+            'departure_station' => 'bologna',
+            'arrival_station' => 'bari',
+            'departure_time' => '00:00:00',
+            'arrival_time' => '00:01:01',
+            'train_code' => 12345,
+            'number_of_carriages' => 1234,
+            'in_time' => 1,
+            'deleted' => 0,
+
+        ];
+
+        foreach ($trainsssssss as $treno) {
+            $TrainClass = new Train();
+        }
     }
 }
