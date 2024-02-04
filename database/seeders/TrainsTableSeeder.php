05c0ada53bdacd180097711ce23e803123e2fe3c
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-04 11:23:57 +0100

    test in TrainsTableSeeder per inserire dati con array

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -12,21 +12,47 @@ class TrainsTableSeeder extends Seeder
 
     public function run(): void
     {
-        $trainsssssss = [
-            'id' => 1,
-            'company' => 'trenitalia',
-            'departure_station' => 'bologna',
-            'arrival_station' => 'bari',
-            'departure_time' => '00:00:00',
-            'arrival_time' => '00:01:01',
-            'train_code' => 12345,
-            'number_of_carriages' => 1234,
-            'in_time' => 1,
-            'deleted' => 0,
+        $trains = [
+            [
+                'id' => 1,
+                'company' => 'trenitalia',
+                'departure_station' => 'bologna',
+                'arrival_station' => 'bari',
+                'departure_time' => '00:00:00',
+                'arrival_time' => '00:01:01',
+                'train_code' => 12345,
+                'number_of_carriages' => 1234,
+                'in_time' => 1,
+                'deleted' => 0,
+            ],
+            [
+                'id' => 1,
+                'company' => 'trenitalia',
+                'departure_station' => 'mantova',
+                'arrival_station' => 'livorno',
+                'departure_time' => '00:00:00',
+                'arrival_time' => '00:01:01',
+                'train_code' => 12765,
+                'number_of_carriages' => 1234,
+                'in_time' => 1,
+                'deleted' => 0,
+            ],
+            [
+                'id' => 1,
+                'company' => 'trenitalia',
+                'departure_station' => 'terni',
+                'arrival_station' => 'salerno',
+                'departure_time' => '00:00:00',
+                'arrival_time' => '00:01:01',
+                'train_code' => 12315,
+                'number_of_carriages' => 1234,
+                'in_time' => 1,
+                'deleted' => 0,
+            ],
 
         ];
 
-        foreach ($trainsssssss as $treno) {
+        foreach ($trains as $treno) {
             $TrainClass = new Train();
         }
     }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,17 +10,24 @@ esercizio
 
 @forelse ($trains as $train)
 
-    <div class="train_info mb-3">
-        <p>{{ $train->company }}</p>
-        <p>{{ $train->departure_station }}</p>
-        <p>{{ $train->arrival_station }}</p>
-        <p>{{ $train->departure_time }}</p>
-        <p>{{ $train->arrival_time}}</p>
-        <p>{{ $train->train_code }}</p>
-        <p>{{ $train->number_of_carriages}}</p>
-        <p>{{ $train->in_time }}</p>
-        <p>{{ $train->deleted }}</p>  
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3">
+            <p>{{ $train->company }}</p>
+            <p>{{ $train->departure_station }}</p>
+            <p>{{ $train->arrival_station }}</p>
+            <p>{{ $train->departure_time }}</p>
+            <p>{{ $train->arrival_time}}</p>
+            <p>{{ $train->train_code }}</p>
+            <p>{{ $train->number_of_carriages}}</p>
+            <p>{{ $train->in_time }}</p>
+            <p>{{ $train->deleted }}</p>  
+        </div>
+        
+        
     </div>
+</div>
+    
 
 @empty
 <p>nessun treno trovato</p>
