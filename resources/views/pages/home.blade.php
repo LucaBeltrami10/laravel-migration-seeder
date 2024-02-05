dffc61895d18e6b1e2a9c97726f617c06b9e7b1a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-05 10:35:32 +0100

    corretto forelse in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,31 +8,36 @@ esercizio
 @section('main-section')
 <p>main</p>
 
-@forelse ($trains as $train)
+
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-3">
-            <p>{{ $train->company }}</p>
-            <p>{{ $train->departure_station }}</p>
-            <p>{{ $train->arrival_station }}</p>
-            <p>{{ $train->departure_time }}</p>
-            <p>{{ $train->arrival_time}}</p>
-            <p>{{ $train->train_code }}</p>
-            <p>{{ $train->number_of_carriages}}</p>
-            <p>{{ $train->in_time }}</p>
-            <p>{{ $train->deleted }}</p>  
-        </div>
+        @forelse ($trains as $train)
+            <div class="col-3">
+                <p>{{ $train->company }}</p>
+                <p>{{ $train->departure_station }}</p>
+                <p>{{ $train->arrival_station }}</p>
+                <p>{{ $train->departure_time }}</p>
+                <p>{{ $train->arrival_time}}</p>
+                <p>{{ $train->train_code }}</p>
+                <p>{{ $train->number_of_carriages}}</p>
+                <p>{{ $train->in_time }}</p>
+                <p>{{ $train->deleted }}</p>  
+            </div>
+        @empty
+            <div class="col-12">
+                <p>nessun treno trovato</p>
+            </div>
+
+    
+        @endforelse
         
         
     </div>
 </div>
     
 
-@empty
-<p>nessun treno trovato</p>
-    
-@endforelse
+
 
 
 @endsection
